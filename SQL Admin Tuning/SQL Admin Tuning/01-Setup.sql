0947fe4b0704f04cb2cde5cb4ec0642930cd2471
Rauchendraser <andreasr@ppedv.de>
2023-02-20 11:03:09 +0100

    Setup

---
+++ SQL Admin Tuning/SQL Admin Tuning/01-Setup.sql
@@ -0,0 +1,34 @@
+
+
+--Installation
+
+--Instanzen
+
+Volumewartungstask..
+
+Master: Logins,Konfig, DB
+model: 	Vorlage für neue DBs
+msdb:  Agent (Jobs, Zeitpläne, Verlauf)	+ Mailsystem
+
+Trenne Daten von Logfiles pro DB überlegen
+
+tempdb: eig HDDs sein (Log von Daten trennen)
+		#tab
+		Auslagerungen
+		Zeilenversionierung Schreiben hindert Lesen nicht
+		IX Wartung
+
+		Anzahl der Datendateien = Anzahl der Kerne max 8
+		-T1117	alle DAteien gleich groß  autom Wachsen
+		-T1118 uniform extents  eine Tabelle nur pro Block (latch)
+
+
+	MAXDOP: Anzahl der Kerne , nicht mehr als 8
+
+	Grenzwerte: 50 OLTP   25 OLAP  
+	MAXDOP = ANzahl der Kerne
+
+
+	MAX RAM: gilt sofort
+	MIN RAM : gilt ab erreicht 
+
